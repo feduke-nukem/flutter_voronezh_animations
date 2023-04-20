4b83a8005393fd4ca9a4a8a84cd3f3293346e6f5
Fedor Blagodyr <bagotir@gmail.com>
2023-04-21 02:01:46 +0400

    upd

--- lib/util/multiply_animation.dart
+++ lib/util/multiply_animation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 /// Анимация, которая производит умножение значений переданных анимаций.
+///
+/// Идея позаимствована из https://youtu.be/YqzUIGsAJQA?t=4831
 class MultiplyAnimation extends CompoundAnimation<double> {
   MultiplyAnimation({required super.first, required super.next});
 
